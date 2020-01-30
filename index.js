0b48ad9d3226df9fd90d26b27ae7a7ea4ae85491
Baktiyar Ishenov <b1963826@gmail.com>
2020-01-30 19:01:47 +0600

    final

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 8000;
 
+app.get('/', (req, res) => {
+	res.send("We're in main page");
+});
+
 app.get('/:something', (req, res) => {
 	res.send(req.params.something);
 });
